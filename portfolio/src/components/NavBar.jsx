07f2e680f6ee0648f7f65b557af68c1a7083a7c1
Katya Weicht <katya.weicht@gmail.com>
2026-03-21 18:04:28 -0600

    Create and style navigation bar

--- portfolio/src/components/NavBar.jsx
+++ portfolio/src/components/NavBar.jsx
@@ -1,8 +1,13 @@
+import {NavLink} from "react-router-dom"
+import '../styles/navbar.css'
+
 function NavBar() {
   return (
     <nav className='navbar'>
       <div className='nav-links'>
-
+        <NavLink to='/' className={({ isActive }) => isActive ? "nav-link active" : "nav-link" } >Katya Weicht</NavLink>
+        <NavLink to='/projects' className={({ isActive }) => isActive ? "nav-link active" : "nav-link" }>Projects</NavLink>
+        <NavLink to='/articles' className={({ isActive }) => isActive ? "nav-link active" : "nav-link" } >Articles</NavLink>
       </div>
     </nav>
   )
