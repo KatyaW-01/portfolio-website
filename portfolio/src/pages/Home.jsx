f58b95afbccc0a7d34f0838094984c81c2533b77
Katya Weicht <katya.weicht@gmail.com>
2026-03-21 18:03:38 -0600

    Update home component

--- portfolio/src/pages/Home.jsx
+++ portfolio/src/pages/Home.jsx
@@ -4,15 +4,7 @@ function Home() {
   return (
     <div className='home-component'>
       <NavBar />
-      <div className='home-div'>
-        {/* render component here */}
-      </div>
-      <div className='home-div-two'>
-         {/* render component here */}
-      </div>
-      <div className='home-div-three'>
-          {/* render component here */}
-      </div>
+      <p>home page with about me</p>
     </div>
   )
 }
