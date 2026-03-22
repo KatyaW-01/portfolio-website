e7dbcb4f3b829979428a76142228cda24e0ae87c
Katya Weicht <katya.weicht@gmail.com>
2026-03-21 18:01:59 -0600

    Create articles and projects components

---
+++ portfolio/src/pages/Articles.jsx
@@ -0,0 +1,12 @@
+import NavBar from "../components/NavBar"
+
+function Articles() {
+  return (
+    <div className='articles-component'>
+      <NavBar/>
+      <p>links to Medium articles go here</p>
+    </div>
+  )
+}
+
+export default Articles
---
+++ portfolio/src/pages/Projects.jsx
@@ -0,0 +1,12 @@
+import NavBar from "../components/NavBar"
+
+function Projects() {
+  return (
+    <div className='projects-component'>
+      <NavBar />
+      <p>links and visuals of notable projects go here</p>
+    </div>
+  )
+}
+
+export default Projects
