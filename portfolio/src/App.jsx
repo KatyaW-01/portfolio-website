46db8551cf5ac3ad476d89c1e8cb5ed196e44984
Katya Weicht <katya.weicht@gmail.com>
2026-03-21 18:03:12 -0600

    Add routes for projects and articles and update background styling

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,6 +1,8 @@
 import './App.css'
 import {Routes, Route} from "react-router-dom"
 import Home from './pages/Home'
+import Articles from './pages/Articles'
+import Projects from './pages/Projects'
 
 
 function App() {
@@ -8,6 +10,8 @@ function App() {
     <div className='app-div'>
       <Routes>
         <Route path='/' element={<Home />}/>
+        <Route path='/projects' element={<Projects />}/>
+        <Route path='/articles' element={<Articles />}/>
       </Routes>
     </div>
   )
